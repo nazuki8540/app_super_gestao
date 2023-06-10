101a00bb9c400bebf54b9ca4ddae8ad110228e6d
Matheus Ghotme <51427002+nazuki8540@users.noreply.github.com>
2023-06-09 23:04:14 -0300

    create routes

--- routes/web.php
+++ routes/web.php
@@ -18,7 +18,6 @@ Route::middleware('autenticacao:ldap')
         //inicio
         Route::get('/home', [\App\Http\Controllers\HomeController::class,'index'])->name('app.home');
         Route::get('/sair', [\App\Http\Controllers\LoginController::class,'sair'])->name('app.sair');
-        Route::get('/cliente', [\App\Http\Controllers\ClienteController::class,'index'])->name('app.cliente');
         //fornecedor
         Route::get('/fornecedor', [\App\Http\Controllers\FornecedorController::class,'index'])->name('app.fornecedor');
         Route::post('/fornecedor/listar', [\App\Http\Controllers\FornecedorController::class,'listar'])->name('app.fornecedor.listar');
@@ -31,6 +30,10 @@ Route::middleware('autenticacao:ldap')
         Route::resource('produto', \App\Http\Controllers\ProdutoController::class);
         //produto detalhes
         Route::resource('produto-detalhe', \App\Http\Controllers\ProdutoDetalheController::class);
+
+        Route::resource('cliente', \App\Http\Controllers\ClienteController::class);
+        Route::resource('pedido', \App\Http\Controllers\PedidoController::class);
+        Route::resource('pedido-produto', \App\Http\Controllers\PedidoProdutoController::class);
 });
 
 Route::get('/teste/{p1}/{p2}',[\App\Http\Controllers\TesteController::class,'teste'])->name('teste');
